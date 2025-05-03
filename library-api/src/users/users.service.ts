1fb00bcf32727879706fc4d59a87c424d757bad2
noomsloof <y.foolsmoon@gmail.com>
2025-05-03 17:02:36 +0700

    532025

--- library-api/src/users/users.service.ts
+++ library-api/src/users/users.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { db } from '../firebase';
 
 @Injectable()
 export class UsersService {
 
-  private users = [
-    { id: 1, name: 'Adam' },
-    { id: 2, name: 'Bob' },
-  ]
+  // private users = [
+  //   { id: 1, name: 'Adam' },
+  //   { id: 2, name: 'Bob' },
+  // ]
 
-  create(createUserDto: CreateUserDto) {
-    const newUser = {
-      id: this.users.length+1,
-      ...createUserDto,
-    };
-    this.users.push(newUser);
-    return newUser;
+  private usersCollection = db.collection('users')
+
+  async create(createUserDto: CreateUserDto) {
+    // const newUser = {
+    //   id: this.users.length+1,
+    //   ...createUserDto,
+    // };
+    // this.users.push(newUser);
+    // return newUser;
+
+    const docRef = await this.usersCollection.add(createUserDto);
+    return {id: docRef.id, ...createUserDto}
+    
   }
 
-  findAll() {
-    return this.users;
+  async findAll() {
+    const snapshot = await this.usersCollection.get();
+    return snapshot.docs.map(doc => ({id : doc.id, ...doc.data() }));
   }
 
   findOne(id: number) {
